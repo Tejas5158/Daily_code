b9793b06edc1b2b86572586dd6857e21177e45c0
tejas5158 <kadamtejas9236@gmail.com>
2026-02-06 15:20:16 +0530

    Student records inserted successfully!

---
+++ stud_teach.sql
@@ -0,0 +1,15 @@
+create database Pract;
+use pract;
+
+create table student(roll_no int primary key,s_name char(10),class varchar(5));
+
+create table teacher(id int primary key,Tname char(10),Age int,Subject char(10)
+,roll_no int,foreign key(roll_no) references student(roll_no));
+
+select *
+from student;
+
+select *
+from teacher;
+
+insert into student values(11,'Mangesh','10 B'); 
